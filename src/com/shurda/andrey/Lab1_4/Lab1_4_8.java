cc23e390da58bf04f23370190352ace68d95a304
Andrii Shurda <adriano225@mal.ru>
2017-01-27 11:35:50 +0200

    Edit Lab1_4_8

--- src/com/shurda/andrey/Lab1_4/Lab1_4_8.java
+++ src/com/shurda/andrey/Lab1_4/Lab1_4_8.java
@@ -1,17 +1,16 @@
 package com.shurda.andrey.Lab1_4;
 
 /**
- * Created by admin on 14.01.2017.
+ Write a console program that prints result type casting for different primitive types.
  */
 public class Lab1_4_8 {
     public static void main(String[] args) {
         int a = 258;
         byte b = (byte) a;
-        long l= 3_000_000_000L;
-        int c=(int)l;
+        long l = 3_000_000_000L;
+        int c = (int) l;
 
-        System.out.println(b);
-        System.out.println(c);
-
+        System.out.println("Int " + a + " cast to byte" + b);
+        System.out.println("Long " + l + " cast to int" + c);
     }
 }
