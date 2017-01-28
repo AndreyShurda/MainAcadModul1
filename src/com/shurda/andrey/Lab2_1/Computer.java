ca36c21875bf559cbefd55867b9623affd86192b
Shurda Andrii <adriano225@mail.ru>
2017-01-28 13:23:24 +0200

    Lab 2 use class

---
+++ src/com/shurda/andrey/Lab2_1/Computer.java
@@ -0,0 +1,82 @@
+package com.shurda.andrey.Lab2_1;
+
+/**
+ *
+ * Write Computer class, the attributes of this class is manufacturer (of String type),
+ * serialNumber (of int type), price (of float type),
+ * quantityCPU (of int type) and frequencyCPU (of int type).
+ *
+ * Add to class Computer method void view(){} that prints all fields of object in line.
+ * Print all info (fields) of all objects in console.
+ */
+public class Computer {
+    private String manufacture;
+    private int serialNumber;
+    private float price;
+    private int quantityCPU;
+    private int frequencyCPU;
+
+    public Computer() {
+    }
+
+    public Computer(String manufacture, int serialNumber, int quantityCPU, int frequencyCPU) {
+        this.manufacture = manufacture;
+        this.serialNumber = serialNumber;
+        this.quantityCPU = quantityCPU;
+        this.frequencyCPU = frequencyCPU;
+    }
+
+    public String getManufacture() {
+        return manufacture;
+    }
+
+    public void setManufacture(String manufacture) {
+        this.manufacture = manufacture;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantityCPU() {
+        return quantityCPU;
+    }
+
+    public void setQuantityCPU(int quantityCPU) {
+        this.quantityCPU = quantityCPU;
+    }
+
+    public int getFrequencyCPU() {
+        return frequencyCPU;
+    }
+
+    public void setFrequencyCPU(int frequencyCPU) {
+        this.frequencyCPU = frequencyCPU;
+    }
+
+    public void view(){
+        System.out.println(toString());
+    }
+    @Override
+    public String toString() {
+        return "Computer{" +
+                "manufacture='" + manufacture + '\'' +
+                ", serialNumber=" + serialNumber +
+                ", price=" + price +
+                ", quantityCPU=" + quantityCPU +
+                ", frequencyCPU=" + frequencyCPU +
+                '}';
+    }
+}
