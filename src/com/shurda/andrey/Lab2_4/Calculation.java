70ab55b9f1017bc521df3eb2f2855c85f994a760
Andrii Shurda <adriano225@mal.ru>
2017-01-30 11:15:48 +0200

    Lab2_4_1-3

---
+++ src/com/shurda/andrey/Lab2_4/Calculation.java
@@ -0,0 +1,14 @@
+package com.shurda.andrey.Lab2_4;
+
+import java.util.Arrays;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int[] ar = {2, 8, 3, 9, -1, 8, 10, 6};
+
+        System.out.println(Arrays.toString(ar));
+        System.out.println("The minimum element is " + MyMath.findMin(ar));
+        System.out.println("The maximum element is " + MyMath.findMax(ar));
+        System.out.println("The area of circle is " + MyMath.areaOfCircle(2.5));
+    }
+}
