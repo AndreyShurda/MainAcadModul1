6232dd487fb128cccdffe61c2cf6992310ee1c13
Shurda Andrii <adriano225@mail.ru>
2017-03-05 14:01:02 +0200

    Lab2-9-1 modify

---
+++ src/com/shurda/andrey/Lab2_9/com/brainacad/oop/teststring2/Main.java
@@ -0,0 +1,19 @@
+package com.shurda.andrey.Lab2_9.com.brainacad.oop.teststring2;
+
+import java.util.StringTokenizer;
+
+/**
+ * Create a class Main with a main() method. In method main() declare two local variables myStr1 and myStr2 of String type and assign a value “Cartoon” for first string and ”Tomcat” for second. Write code to display all of the letters, which are present in the first word, but absent in the second.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String myStr1 = "Cartoon";
+        String myStr2 = "Tomcat";
+
+        StringTokenizer s = new StringTokenizer(myStr1, myStr2);
+        while (s.hasMoreElements()) {
+            System.out.print(s.nextElement() + " ");
+        }
+
+    }
+}
