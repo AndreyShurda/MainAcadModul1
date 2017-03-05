ba503c3a8e13acb4ce9bf1120d2882345042c28b
Shurda Andrii <adriano225@mail.ru>
2017-03-05 14:02:58 +0200

    Lab2-9-5

---
+++ src/com/shurda/andrey/Lab2_9/com/brainacad/oop/regexs/Main.java
@@ -0,0 +1,23 @@
+package com.shurda.andrey.Lab2_9.com.brainacad.oop.regexs;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Create new project called TestRegExp. Add package “com.brainacad.oop.testsregexp”. Create a class Main with a main() method. Create public static method checkPersonWithRegExp(String userNameString) which take String as argument and return boolean value. This method must return true only if argument string value begin from Uppercase, then other chars in Lowercase and contains only letters. In main() method declare array of String with next values: {“VOVA”,”Ivan”,”R2d2”,”ZX”,”Anna”,”12345”,”ToAd”,”TomCat”,” “} Add code to check each of these values with checkPersonWithRegExp() method and print result to console.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String[] arr = {"VOVA", "Ivan", "R2d2", "ZX", "Anna", "12345", "ToAd", "TomCat", ""};
+
+        for (String s : arr) {
+            System.out.println(s + " is " + checkPersonWithRegExp(s));
+        }
+    }
+
+    public static boolean checkPersonWithRegExp(String userNameString) {
+        Pattern p = Pattern.compile("^[A-Z][a-z]+");
+        Matcher m = p.matcher(userNameString);
+        return m.matches();
+    }
+}
