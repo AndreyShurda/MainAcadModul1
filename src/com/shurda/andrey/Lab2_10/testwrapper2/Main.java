48342ce9cd2748a93402a8efee9ef8309dbe4060
Shurda Andrii <adriano225@mail.ru>
2017-03-05 14:04:58 +0200

    Lab2-10-2

---
+++ src/com/shurda/andrey/Lab2_10/testwrapper2/Main.java
@@ -0,0 +1,20 @@
+package com.shurda.andrey.Lab2_10.testwrapper2;
+
+/**
+ * Add to class Main static method compute( , ) that use as argument s two different numeric wrapper classes,
+ * adds the values of the two instances together,
+ * and then creates a third numeric wrapper instance whose value is the sum and return it.
+ * For example: static method compute(Byte, Integer) witch return Long type.
+ * In method main() write code to invoke compute(,) and print result to console.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Byte myBate = 0b011;
+        Integer myInteger = Integer.valueOf(487);
+        System.out.println("Sum Byte and Integer " + compute(myBate, myInteger));
+    }
+
+    public static Long compute(Byte b, Integer i) {
+        return new Long(b + i);
+    }
+}
