a1c7f09ea1ba1d0c19192469a472b7486fafbb96
AndreyShurda <adriano225@mail.ru>
2017-03-06 16:10:56 +0200

    Lab2-9-3 write good code

--- src/com/shurda/andrey/Lab2_9/com/brainacad/oop/teststring3/Main.java
+++ src/com/shurda/andrey/Lab2_9/com/brainacad/oop/teststring3/Main.java
@@ -1,5 +1,9 @@
 package com.shurda.andrey.Lab2_9.com.brainacad.oop.teststring3;
 
+import com.shurda.andrey.Util.ArrayElements;
+
+import java.util.Arrays;
+
 /**
  * Create a class Main with a main() method.
  * Create static method: uniqueChars(String s),
@@ -8,24 +12,26 @@ package com.shurda.andrey.Lab2_9.com.brainacad.oop.teststring3;
  */
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(uniqueChars("Using methods of class String"));
-        System.out.println("Using methods of class String".chars().distinct());
+        System.out.println("Find unique chars in this string: ");
+        System.out.println("Using methods of class String");
+        System.out.println("The unique chars is: ");
+        System.out.println(uniqueChars("Using methods of class String"));
     }
 
-    public static void uniqueChars(String s) {
-        int count = 0;
+    public static char[] uniqueChars(String s) {
         char[] chars = s.toCharArray();
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < chars.length - 1; i++) {
-            for (int j = i; j < chars.length; j++) {
-                if (chars[i] == chars[j]){
 
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j < chars.length; j++) {
+                if (i != j && chars[i] == chars[j]) {
+                    break;
+                }
+                if (j == chars.length - 1) {
+                    sb.append(chars[i]);
                 }
             }
         }
-//        }
-
-//        return chars;
-
+        return sb.toString().toCharArray();
     }
 }
