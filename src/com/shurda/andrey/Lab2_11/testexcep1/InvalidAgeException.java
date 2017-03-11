f6b4df20de9c7b313eb889e18c02313d3fb66f5e
Shurda Andrii <adriano225@mail.ru>
2017-03-11 13:19:35 +0200

    Lab2-11-3

---
+++ src/com/shurda/andrey/Lab2_11/testexcep1/InvalidAgeException.java
@@ -0,0 +1,7 @@
+package com.shurda.andrey.Lab2_11.testexcep1;
+
+public class InvalidAgeException extends RuntimeException {
+    public InvalidAgeException(String s) {
+        super(s);
+    }
+}
---
+++ src/com/shurda/andrey/Lab2_11/testexcep1/Person.java
@@ -0,0 +1,55 @@
+package com.shurda.andrey.Lab2_11.testexcep1;
+
+/**
+ * Create class Main with method main().
+ * Create class Person with fields firstName (of String type), lastName (String), age (int).
+ * Add to class Person setters and getters for each field.
+ * Create InvalidAgeException class  that is subclass of RuntimeException.
+ * Setter in class Person which will modify age (setAge(int )) should throw InvalidAgeException when age out of range 1-120.
+ * Add code to method main() to create one Pearson object and invoke setAge()
+ * method with exception handling using try-catch statement
+ */
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 1 && age < 121) {
+            this.age = age;
+        } else {
+            throw new InvalidAgeException("Age not valid 1 to 120");
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+        int age = 122;
+        try {
+            person.setAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println(age + " " + e.getMessage());
+        }
+
+    }
+}
