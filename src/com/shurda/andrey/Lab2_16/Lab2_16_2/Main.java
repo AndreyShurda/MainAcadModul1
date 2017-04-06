eed63eef8e04c625890de091835189942c90c677
AndriiShurda <adriano225@mail.ru>
2017-04-06 15:09:02 +0300

    Lab2_16-3

--- src/com/shurda/andrey/Lab2_16/Lab2_16_2/Main.java
+++ src/com/shurda/andrey/Lab2_16/Lab2_16_2/Main.java
@@ -1,7 +1,11 @@
 package com.shurda.andrey.Lab2_16.Lab2_16_2;
 
 /**
- * Created by admin on 06.04.2017.
+ * Student should create a new blank Java Project in IDE. • Student should create his or
+ * her own Java class Main with methods void foo(int i)
+ * which print “int” to console and void foo(Byte b) which print “Byte”;
+ * In a method main() student should create variable (byte b) and put it
+ * to method (foo) and look what will be output
  */
 public class Main {
     public static void main(String[] args) {
---
+++ src/com/shurda/andrey/Lab2_16/Lab2_16_3/Main.java
@@ -0,0 +1,23 @@
+package com.shurda.andrey.Lab2_16.Lab2_16_3;
+
+import java.util.Arrays;
+
+/**
+ * Student should create his or her own Java class Main with methods  void foo(int a, int b)
+ * which print a and b to console and void foo(int … a) which print array’s content;
+ * In a method main() student should put two and three integers to method (foo) and look for result.
+ */
+public class Main {
+    public static void main(String[] args) {
+        foo(1, 2);
+        foo(1, 2, 3);
+    }
+
+    public static void foo(int a, int b) {
+        System.out.println("a = " + a + " b = " + b);
+    }
+
+    public static void foo(int... arg) {
+        System.out.println(Arrays.toString(arg));
+    }
+}
