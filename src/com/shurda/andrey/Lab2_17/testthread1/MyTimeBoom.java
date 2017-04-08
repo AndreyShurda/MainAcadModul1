12356de4c9563c6fcebcbcc9bc5ff8c0e8164e89
AndreyShurda <adriano225@mail.ru>
2017-04-08 21:42:09 +0300

    Lab2_17_1

---
+++ src/com/shurda/andrey/Lab2_17/testthread1/MyTimeBoom.java
@@ -0,0 +1,17 @@
+package com.shurda.andrey.Lab2_17.testthread1;
+
+
+public class MyTimeBoom extends Thread {
+    @Override
+    public void run() {
+        try {
+            for (int i = 10; i > 0; i--) {
+                Thread.sleep(1000);
+                System.out.println(i);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Boom!!!");
+    }
+}
