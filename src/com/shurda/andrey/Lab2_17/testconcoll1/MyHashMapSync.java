ba726225290a5c4130223313fd64c725efe7b277
AndriiShurda <adriano225@mail.ru>
2017-04-21 08:56:50 +0300

    Lab2_17-10

---
+++ src/com/shurda/andrey/Lab2_17/testconcoll1/MyHashMapSync.java
@@ -0,0 +1,22 @@
+package com.shurda.andrey.Lab2_17.testconcoll1;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MyHashMapSync<K,V> extends HashMap<K,V>{
+//    @Override
+//    public synchronized V put(K key, V value) {
+//        return super.put(key, value);
+//    }
+//
+//    @Override
+//    public synchronized V get(Object key) {
+//        return super.get(key);
+//    }
+
+//    @Override
+//    public synchronized Set<Entry<K, V>> entrySet() {
+//        return super.entrySet();
+//    }
+}
