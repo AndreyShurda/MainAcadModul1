5cc96518e3abce1cded7137cd7bb98def0f22d7d
AndreyShurda <adriano225@mail.ru>
2017-05-06 13:40:36 +0300

    SE_Lab1-7-5 finished

--- src/com/shurda/andrey/se/Lab1_7/tetsapidate/Main.java
+++ src/com/shurda/andrey/se/Lab1_7/tetsapidate/Main.java
@@ -1,10 +1,9 @@
 package com.shurda.andrey.se.Lab1_7.tetsapidate;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.Period;
+import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 
 /**
  * Add package “com.brainacad.oop.testapidate”. Create a class Main with a main() method, which:
@@ -20,31 +19,60 @@ public class Main {
         System.out.println("Date now is: " + localDate);
 
         LocalDate myBirthday = LocalDate.of(1989, 07, 22);
-        int myYears = localDate.getYear() - myBirthday.getYear();
+        System.out.println("My birthday: " + myBirthday);
 
-        long yearsBetween = ChronoUnit.YEARS.between(myBirthday, localDate);
-        System.out.println("I am " + yearsBetween);
-        if (localDate.getMonth().getValue() < myBirthday.getMonth().getValue()) {
-            myYears--;
-        }
-
+        Period myPeriod = Period.between(myBirthday, localDate);
+        int myYears = myPeriod.getYears();
         System.out.println("I am " + myYears);
 
-        System.out.println("My birthday: " + myBirthday);
-//        System.out.println("My birthday week: " + myBirthday.getDayOfWeek());
+
         System.out.println("I was born on day of the week: " + myBirthday.getDayOfWeek());
-        System.out.println("My birthday in this year will be on day of the week: " + myBirthday.plusYears(28).getDayOfWeek());
+        System.out.println("My birthday in this year will be on day of the week: " + myBirthdayDayOfWeek(myBirthday));
+
+        System.out.print("My Birthday in this year: ");
+        isBirthDayWas(myBirthday);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy, HH-mm-ss");
 
         LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(localDateTime.format(formatter));
+        System.out.println("Now is: " + localDateTime.format(formatter));
 
-        Period between = Period.between(myBirthday,localDate);
+        ZoneId kiev = ZoneId.of("Europe/Kiev");
+        ZoneId berlin = ZoneId.of("Europe/Berlin");
+        ZonedDateTime kievZonedDateTime = ZonedDateTime.of(localDateTime, kiev);
+        ZonedDateTime berlinZonedDateTime = kievZonedDateTime.withZoneSameInstant(berlin);
 
-        System.out.println("Period");
-        System.out.println(between.plusYears(1));
-        System.out.println(between.getDays());
-        System.out.println(between);
+        System.out.println("In Kiev now is: " + kievZonedDateTime.format(formatter));
+        System.out.println("In Berlin now is: " + berlinZonedDateTime.format(formatter));
+    }
+
+    public static void isBirthDayWas(LocalDate birthday) {
+        LocalDate localDate = LocalDate.now();
+        LocalDate birthdayInThisYear = birthdayInThisYear(birthday);
+        Period period = Period.between(localDate, birthdayInThisYear);
+
+        if (period.isNegative()) {
+            System.out.println("past already");
+        } else if (period.isZero()) {
+            System.out.println("birthday today");
+        } else {
+            System.out.println("will be soon");
+        }
+    }
+
+    public static DayOfWeek myBirthdayDayOfWeek(LocalDate birthday) {
+        LocalDate birthdayInThisYear = birthdayInThisYear(birthday);
+
+        return birthdayInThisYear.getDayOfWeek();
+    }
+
+    private static LocalDate birthdayInThisYear(LocalDate birthday) {
+        LocalDate localDate = LocalDate.now();
+
+        int year = localDate.getYear();
+        int month = birthday.getMonthValue();
+        int dayOfMonth = birthday.getDayOfMonth();
+
+        return LocalDate.of(year, month, dayOfMonth);
     }
 }
