96d40865f5051c2c1cf07e5dcfb29b5fc98878d1
AndriiShurda <adriano225@mail.ru>
2017-06-06 21:50:59 +0300

    SE Lab1-8-4 comments

--- src/com/shurda/andrey/se/Lab1_8/datagram/MySender.java
+++ src/com/shurda/andrey/se/Lab1_8/datagram/MySender.java
@@ -8,9 +8,21 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/**
+ * Add package “com.brainacad.datagram”.
+ * 1) Create a class MySender with a main() method, which:
+ * - enters the file name for his transmission over the network;
+ * - creates a socket connection on the basis of DatagramSocket class and organizes packet for transmission in the form
+ * of an instance of a DatagramPacket class.
+ * 2) Create a class MyRecipient with a main() method, which:
+ * - creates a buffer for receiving data packets and a file for writing data;
+ * - creates a socket connection on the basis of DatagramSocket class and organizes the wait loop of data packets,
+ * which receives the data and writes it to a file.
+ * 3) Execute the programs.
+ */
+
 public class MySender {
     public static void main(String[] args) throws IOException {
-//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream("UDP.txt")));
         DatagramSocket client = new DatagramSocket();
         InetAddress inetAddress = InetAddress.getByName("localhost");
